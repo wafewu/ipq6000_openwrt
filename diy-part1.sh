1e3c43f9d2110e2e88e862f9b2c31a07c1724469
wafewu <148876122+wafewu@users.noreply.github.com>
2024-04-09 13:52:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,23 @@
 #sed -i '/packages/d' feeds.conf.default
 #sed -i '/luci/d' feeds.conf.default
 #sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+# git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+
+
+
+# 添加额外软件包
+# Adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+# MosDNS
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+# 科学上网
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/kenzok8/small
+
+# Themes
